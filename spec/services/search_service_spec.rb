006292b4463121dc193e5a2bb3ec1797e23068a9
Izabellyrb <izabelly.rb@hotmail.com>
2024-12-21 02:24:53 -0300

    fix rubocop

--- spec/services/search_service_spec.rb
+++ spec/services/search_service_spec.rb
@@ -3,16 +3,22 @@
 require "rails_helper"
 
 RSpec.describe SearchService do
-  let!(:feedback_response) { create(:feedback_response) }
-  let!(:feedback_response2) { create(:feedback_response, name: "Jane") }
-  let!(:feedback_response3) { create(:feedback_response, name: "Anne", location: "Poá") }
+  let(:feedback_response) { create(:feedback_response) }
+  let(:second_feedback_response) { create(:feedback_response, name: "Jane") }
+  let(:third_feedback_response) { create(:feedback_response, name: "Anne", location: "Poá") }
+
+  before do
+    feedback_response
+    second_feedback_response
+    third_feedback_response
+  end
 
   describe "#call" do
     context "when no parameters are passed" do
       let(:result) { described_class.new({}, nil).call }
 
       it { expect(result.count).to eq(3) }
-      it { expect(result.first).to eq(feedback_response3) }
+      it { expect(result.first).to eq(third_feedback_response) }
       it { expect(result.last).to eq(feedback_response) }
     end
 
@@ -20,7 +26,7 @@ RSpec.describe SearchService do
       subject(:result) { described_class.new({ location: "brasília" }, nil).call }
 
       it { expect(result.count).to eq(2) }
-      it { expect(result.first).to eq(feedback_response2) }
+      it { expect(result.first).to eq(second_feedback_response) }
       it { expect(result.last).to eq(feedback_response) }
     end
 
@@ -28,7 +34,7 @@ RSpec.describe SearchService do
       subject(:result) { described_class.new({ name: "Jane", location: "brasília" }, nil).call }
 
       it { expect(result.count).to eq(1) }
-      it { expect(result.first).to eq(feedback_response2) }
+      it { expect(result.first).to eq(second_feedback_response) }
     end
   end
 end
